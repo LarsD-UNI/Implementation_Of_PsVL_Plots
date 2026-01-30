d1195ab3538b8f2d1a412957e0ad45d7e3fbac7a
Lars Doll <ge87lup2@cm4login2.cos.lrz.de>
2026-01-30 15:22:53 +0100

    .

---
+++ uniform/jobs_uniform/jobscript_psvl_c08_20.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#SBATCH -J psvl_c08_20
+#SBATCH -D ./
+#SBATCH -o ./%x.%j.%N.out
+#SBATCH -e ./%x.%j.%N.err
+#SBATCH --get-user-env
+#SBATCH --export=NONE
+#SBATCH --clusters=cm4
+#SBATCH --partition=cm4_tiny
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=112
+#SBATCH --time=01:30:00
+
+module load slurm_setup
+module load cmake
+module load gcc
+
+export OMP_NUM_THREADS=112
+export OMP_PLACES=cores
+export OMP_PROC_BIND=close
+
+EXEC="$HOME/Projekte/AutoPas/build/examples/md-flexible/md-flexible"
+YAML="uniform_box_length_20.yaml"
+
+for radius in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0; do
+
+  echo "Radius: $radius"
+
+	for i in {1..5}; do
+
+		echo "Run $i (radius=$radius)"
+
+		$EXEC --yaml-filename "$YAML" --verlet-skin-radius "$radius"
+
+	done
+done
