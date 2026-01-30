406755ec69096b015a37f802fbb41e74d104e60f
Lars Doll <ge87lup2@cm4login2.cos.lrz.de>
2026-01-30 16:47:37 +0100

    falling drop csv

---
+++ fallingDrop/jobs_falling/jobscript_c08_LOG.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#SBATCH -J c08_LOG
+#SBATCH -D ./
+#SBATCH -o ./%x.%j.%N.out
+#SBATCH -e ./%x.%j.%N.err
+#SBATCH --get-user-env
+#SBATCH --export=NONE
+#SBATCH --clusters=cm4
+#SBATCH --partition=cm4_tiny
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=112
+#SBATCH --time=00:07:00
+
+module load slurm_setup
+module load cmake
+module load gcc
+
+export OMP_NUM_THREADS=112
+export OMP_PLACES=cores
+export OMP_PROC_BIND=close
+
+EXEC="$HOME/Projekte/AutoPas/build/examples/md-flexible/md-flexible"
+YAML1="fallingDrop_psvl_c08.yaml"
+YAML1="fallingDrop_vlc_c08.yaml"
+
+for radius in 0.3 0.7; do
+
+  echo "Radius: $radius"
+
+	for i in 1; do
+
+		$EXEC --yaml-filename "$YAML1" --verlet-skin-radius "$radius"
+		$EXEC --yaml-filename "$YAML2" --verlet-skin-radius "$radius"
+
+	done
+done
